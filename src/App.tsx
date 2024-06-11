88b597b34ce40e615f0afaa58759540f6b01ff8c
Wendiguin-Do-Mal <sniffyape@gmail.com>
2024-06-11 19:05:51 -0300

    feat: add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
 import './App.css'
-import Header from './components/Header'
 import Form from './components/Form'
-import Hero from './components/Hero'
-import Tickets from './components/Tickets'
-import About from './components/About'
 
 function App() {
  
 
   return( 
-  < >
+  <>
    <div style={{
           display:"flex",
           flexDirection:"column",
           justifyContent:"center",
           alignItems:"center",
           width:"100vw",
-          gap: "2rem",
-          backgroundColor: "#00bfff",
-
               }}>
-   <Header />
-   <Hero/>
-   <About/>
-   <Tickets/>
    <Form/>
    </div>
-
   </>)
 }
 
