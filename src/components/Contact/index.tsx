17964796e3487fefe920d08fe76e484c3d30a4a7
Wendiguin-Do-Mal <sniffyape@gmail.com>
2024-06-12 19:40:49 -0300

    feat: add map location and icons in contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -5,9 +5,20 @@ function Contact() {
     <section id="section-contact" className="box-default">
         <h2 className="h2-default">Entre em contato!!!</h2>
         <div>
-        <p className="p-default">Fale com a gente pelo <a href="">Whatsapp</a>.</p>
-        <p className="p-default">Pelo nosso <a href="">Instagram</a>.</p>
-        <p className="p-default">ou pelo nosso <a href="">Facebook</a>.</p>
+        <p className="p-default">Entre em contato pelo <a href="">Whatsapp</a>, pelo nosso <a href="">Instagram</a> e pelo nosso <a href="">Facebook</a>.</p>
+      
+          
+        <div>
+          <div className="div-img">
+           <a href="">      <img src="src\assets\whatsapp (3).png" alt="logo" /> </a>
+           <a href="">      <img src="src\assets\instagram (2).png" alt="logo" /> </a>
+           <a href="">      <img src="src\assets\facebook (2).png" alt="logo" /> </a>
+
+
+          </div>
+        </div>
+
+
 
         </div>
     </section>
--- src/components/Location/index.tsx
+++ src/components/Location/index.tsx
@@ -1,20 +1,27 @@
-import "./style.css"
+import "./style.css";
 
 function Location() {
   return (
     <section id="section-location" className="box-default">
-        <h2 className="h2-default">Localização</h2>
-        <h3>O evento sera realizado na:</h3>
-        <div>
-
-        <p className="p-default">Cidade: xxxxxxxxx</p>
-        <p className="p-default">Rua: xxxxxxxxxxx</p>        
-        <p className="p-default">Bairro: xxxxxxxxxxxxxx</p>
-        <p className="p-default">Referencia: xxxxxxxxx perto do xxxxxx</p>
-        
-        </div>
+      <h2 className="h2-default">Localização</h2>
+      <h3>O evento sera realizado na:</h3>
+      <div>
+        <ul>
+          <li className="p-default">Cidade: xxxxxxxxx</li>
+          <li className="p-default">Rua: xxxxxxxxxxx</li>
+          <li className="p-default">Bairro: xxxxxxxxxxxxxx</li>
+          <li className="p-default">Referencia: xxxxxxxxx perto do xxxxxx</li>
+        </ul>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3753.3744942934436!2d-40.27135422401789!3d-19.824101181540613!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xb7ea80a57199cb%3A0xafaa88031571c172!2sOriundi%20Shopping!5e0!3m2!1spt-BR!2sbr!4v1718229551905!5m2!1spt-BR!2sbr"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          loading="lazy"
+        ></iframe>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Location;
